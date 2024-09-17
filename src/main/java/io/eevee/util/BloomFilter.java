95f8ebf611165c2232a37b384271e38ea828daf8
Evelyn Bayes <evelynrbayes@gmail.com>
2024-09-17 10:02:19 +1000

    Final changes to Bloom Filter

--- src/main/java/io/eevee/util/BloomFilter.java
+++ src/main/java/io/eevee/util/BloomFilter.java
@@ -1,15 +1,15 @@
 package io.eevee.util;
 
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-
 
 /**
- * Bloom Filter implementation
+ * Bloom Filter implementation.
  *
- * This is a probabilistic data structure which, with a small memory footprint and low compute cost
+ * This is a probabilistic data structure which with a small memory footprint and low compute cost
  * is able to determine if an object has not been stored in the data structure with certainty or
  * has been stored with a high degree of accuracy.
+ *
+ * The current implementation takes byte arrays as input so any user will need to have selected
+ * an appropriate and consistent approach to serialization to use it.
  *
  * <p>Complexity:
  * <ul>
@@ -18,73 +18,72 @@ import java.util.NoSuchElementException;
  *   <li>Insert - O(1)
  *   <li>Remove - NA
  * </ul>
- *
- * @param <E> the type of the elements with which we are applying tests to
  */
-public class BloomFilter<E> {
+
+public class BloomFilter {
     private boolean[] bitArray;
     private int numHashFunctions;
-    private float targetFalsePositiveRate;
 
-    // Rounded up to the nearest power of two given our other defaults and an expected number of elements of 1000
+    // Rounded up to the nearest power of two given a default of 5 hash functions and targer false positive rate of 3%
     private static int MIN_BIT_ARRAY_SIZE = 8192;
 
     // Taken from Google's Guava implementation
     private static int DEFAULT_NUM_HASH_FUNCTIONS = 5;
-    private static float DEFAULT_FALSE_POSITIVE_RATE = 0.03f;
 
     BloomFilter() {
         bitArray = new boolean[MIN_BIT_ARRAY_SIZE];
         numHashFunctions = DEFAULT_NUM_HASH_FUNCTIONS;
-        targetFalsePositiveRate = DEFAULT_FALSE_POSITIVE_RATE;
     }
 
     BloomFilter(float targetFalsePositiveRate, int targetNumberOfElements) {
-        numHashFunctions = calculateNumberOfHashFunctions(targetFalsePositiveRate);
-        bitArray = new boolean[calculateArraySize(targetFalsePositiveRate, targetNumberOfElements, numHashFunctions)];
-        this.targetFalsePositiveRate = targetFalsePositiveRate;
+        numHashFunctions = optimalNumHashFunctions(targetFalsePositiveRate);
+        bitArray = new boolean[optimalBitArraySize(targetFalsePositiveRate, targetNumberOfElements, numHashFunctions)];
     }
 
     // Visible and strictly available for testing
-    int capacity() {
+    int bitSize() {
         return bitArray.length;
     }
 
+    // Visible and strictly available for testing
+    int bitsSet() {
+        int count = 0;
+        for (boolean b : bitArray) {
+            if (b == true) count++;
+        }
+        return count;
+    }
+
+    // Visible and strictly available for testing
+    int numHashFunctions() {
+        return numHashFunctions;
+    }
+
     /**
-     * Inserts markers indicating the inclusion of the element in the Bloom Filter
+     * Inserts markers indicating the inclusion of the object in the Bloom Filter
      *
      * <p>Complexity: O(1).
      *
-     * @param element element to be appended
+     * @param bytes byte array representation of the object to be added
      */
-    public void add(E element) {
+    public void add(byte[] bytes) {
         for (int i = 0; i < numHashFunctions; i++) {
-            byte[] bytes = hashCodeToByteArray(element.hashCode());
             int hash = Murmur.hash(bytes, i * Murmur.DEFAULT_SEED);
             int index = Math.floorMod(hash, bitArray.length);
             bitArray[index] = true;
         }
     }
 
-    private byte[] hashCodeToByteArray(int value) {
-        return new byte[] {
-            (byte) (value >> 24),
-            (byte) (value >> 16),
-            (byte) (value >> 8),
-            (byte) value};
-    }
-
     /**
-     * Returns false if the element is guaranteed to not exist in the Bloom Filter.
+     * Returns false if the object is guaranteed to not exist in the Bloom Filter.
      *
      * <p>Complexity: O(1).
      *
-     * @param element element to test for inclusion
-     * @return false if the element has not been included in the bloom filter
+     * @param bytes byte array representation of the object
+     * @return false if the object has not been included in the bloom filter
      */
-    public boolean mayContain(E element) {
+    public boolean mayContain(byte[] bytes) {
         for (int i = 0; i < numHashFunctions; i++) {
-            byte[] bytes = hashCodeToByteArray(element.hashCode());
             int hash = Murmur.hash(bytes, i * Murmur.DEFAULT_SEED);
             int index = Math.floorMod(hash, bitArray.length);
             if (bitArray[index] == false) {
@@ -94,20 +93,21 @@ public class BloomFilter<E> {
         return true;
     }
 
-    private int calculateArraySize(float targetFalsePositiveRate, int targetNumberOfElements, int numHashFunctions) {
-        int numerator = - numHashFunctions * targetNumberOfElements;
-        double denominator = Math.log(1 - Math.pow(targetFalsePositiveRate, 1.0f / numHashFunctions));
-        return (int) Math.floor(numerator / denominator);
+    private int optimalBitArraySize(float targetFalsePositiveRate, int targetNumberOfElements, int numHashFunctions) {
+        double requiredBitsPerInsert = - numHashFunctions / Math.log(1 - Math.pow(targetFalsePositiveRate, 1.0f / numHashFunctions));
+        double requiredBits = requiredBitsPerInsert * targetNumberOfElements;
+        return (int) requiredBits;
     }
 
-    private int calculateNumberOfHashFunctions(float targetFalsePositiveRate) {
-        int numHashFunctions = (int) Math.floor(Math.log(targetFalsePositiveRate) / Math.log(2));
-        if (numHashFunctions == 0) {
+    private int optimalNumHashFunctions(float targetFalsePositiveRate) {
+        double optimalNumHashFunctions = - Math.log(targetFalsePositiveRate) / Math.log(2);
+        if (optimalNumHashFunctions < 1) {
             return 1;
-        } else if (numHashFunctions > 256) {
-            return 256;
+        } else if (optimalNumHashFunctions < 256) {
+            // Converts the type and rounds down to the nearest int
+            return (int) optimalNumHashFunctions;
         } else {
-            return numHashFunctions;
+            return 256;
         }
     }
 }
--- src/test/java/io/eevee/util/BloomFilterTest.java
+++ src/test/java/io/eevee/util/BloomFilterTest.java
@@ -1,6 +1,7 @@
 package io.eevee.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.lang.Integer;
 
@@ -12,31 +13,48 @@ import org.junit.jupiter.api.Test;
  */
 public class BloomFilterTest {
 
-    private BloomFilter<Integer> emptyBloomFilter;
-    private BloomFilter<Integer> singleElementBloomFilter;
-
-    @BeforeEach
-    public void setup() {
-        emptyBloomFilter = new BloomFilter<>();
-
-        singleElementBloomFilter = new BloomFilter<>();
-        singleElementBloomFilter.add(0);
+    @Test
+    public void coherenceTest() {
+        BloomFilter bloomFilter = new BloomFilter();
+        for (int i = 0; i < 100; i++) {
+            bloomFilter.add(String.valueOf(i).getBytes());
+            assertTrue(bloomFilter.mayContain(String.valueOf(i).getBytes()));
+        }
     }
 
-    /**
-     * Tests to write:
-     *   1. Coherence check which inserts "x" objects and validates we can find them
-     *   2. Check which inserts "x" objects with "y" known collisions and tests we see this reflected in the number of bits in our array
-     *   3. Check with "x" inserts our false positive rate is within the bounds we expect
-     *   4. Test we build the correct size based on our expected number of inserts and false positive rate
-     *   5. Test we select the correct number of hash functions
-     */
+    @Test
+    public void testForKnownNumberOfCollisions() {
+        BloomFilter bloomFilter = new BloomFilter(0.01f, 10000);
+        for (int i = 0; i < 10000; i++) {
+            bloomFilter.add(String.valueOf(i).getBytes());
+        }
+        assertEquals(44645, bloomFilter.bitsSet());
+    }
 
     @Test
-    public void testMightContainMethod() {
-        assertEquals(false, emptyBloomFilter.mayContain(0));
+    public void testFalsePositiveRateIsWithinExpectedBounds() {
+        BloomFilter bloomFilter = new BloomFilter(0.01f, 10000);
+        for (int i = 0; i < 10000; i++) {
+            bloomFilter.add(String.valueOf(i).getBytes());
+        }
 
-        assertEquals(true, singleElementBloomFilter.mayContain(0));
-        assertEquals(false, singleElementBloomFilter.mayContain(1));
+        int falsePositives = 0;
+        for (int i = 10000; i < 20000; i++) {
+            if (bloomFilter.mayContain(String.valueOf(i).getBytes()) == true) falsePositives++;
+        }
+        assertEquals(97, falsePositives);
+        assertTrue(falsePositives < 101);
+    }
+
+    @Test
+    public void testBitArrayIsOfCorrectSize() {
+        BloomFilter bloomFilter = new BloomFilter(.01f, 10000);
+        assertEquals(96166, bloomFilter.bitSize());
+    }
+
+    @Test
+    public void testOptimalNumberOfHashFunctionsIsSelected() {
+        BloomFilter bloomFilter = new BloomFilter(.01f, 10000);
+        assertEquals(6, bloomFilter.numHashFunctions());
     }
 }
